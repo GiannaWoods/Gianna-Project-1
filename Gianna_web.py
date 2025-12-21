f54f469a53362ca297f9e14f3b655f3787132cb2
GiannaWoods <Gianna.Woods@student.mec.cuny.edu>
2025-12-20 22:54:21 -0500

    Update image in student grade calculator section

--- Gianna_web.py
+++ Gianna_web.py
@@ -107,7 +107,7 @@ elif page == '💼 Projects':
   with st.container():
     col1, col2 = st.columns([1,2])
     with col1:
-      st.image('https://www.publicdomainpictures.net/pictures/90000/nahled/calculator-black-clipart.jpg', use_column_width = True)
+      st.image('1-BIG_FimagePROJ114.jpg', use_column_width = True)
      
     with col2:
       st.subheader('📊 Student Grade Calulator')
